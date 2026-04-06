ee026fff26806fdc22c5fa72512919ce3384c83a
ZENG,ROU-QI <111508016@g.nccu.edu.tw>
2026-04-06 23:32:52 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-blue-300 h-screen w-full flex justify-center items-center">
+        <div className="bg-white p-6 rounded-lg flex justify-center items-center flex-col inline-block">
+          <div>曾柔綺</div>
+          <div>rouqi zeng</div>
+        </div>
+      </div>
+    </>
+  );
+}
